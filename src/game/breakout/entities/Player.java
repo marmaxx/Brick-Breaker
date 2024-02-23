ec089d6dc44fd20fd61d45202c7f85b4e4f5a963
filaudea <marius.filaudeau@etu.u-paris.fr>
2024-02-23 19:55:53 +0100

    maj

--- src/game/breakout/entities/Player.java
+++ src/game/breakout/entities/Player.java
@@ -14,7 +14,7 @@ import game.breakout.entities.rules.Entity;
 
 public class Player extends Entity {
 	public static final Color DEFAULT_COLOR = Color.WHITE;
-	public static final int DEFAULT_SIZE = 200; //the size of the paddle image
+	public static final int DEFAULT_SIZE = 100; //the size of the paddle image
 	public static final int DEFAULT_POS_X = 300;
 	public static final int DEFAULT_POS_Y = 300;
 	public static final int MOVE_STEP = 30;
