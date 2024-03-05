c05cb86b2c60edb294ce7054ac2fb6e14638e42e
DELPECH MAXIME <121643923+MDelpech@users.noreply.github.com>
2024-03-05 08:30:18 +0100

    changement image game over et win panel

--- src/display/view/GameOver.java
+++ src/display/view/GameOver.java
@@ -2,21 +2,29 @@ package display.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 
 public class GameOver extends JPanel{
     public static final Dimension BUTTON_SIZE = new Dimension(300,100); 
     public static final Dimension SCREEN_FULL_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-    private JLabel title = new JLabel(" GAME OVER "); 
     private JButton exit = new JButton(" EXIT ");
+    private BufferedImage backgroundImage; // background image 
 
 
     public GameOver(){
         this.setLayout(new FlowLayout());
         this.setPreferredSize(SCREEN_FULL_SIZE);
         
-         //settings title
-         this.add(this.title);
+
+         try {
+            backgroundImage = ImageIO.read(new File("src/GameOver.jpg")); // Chemin vers votre image
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
          //settings exit button 
          this.exit.setPreferredSize(BUTTON_SIZE);
@@ -24,6 +32,14 @@ public class GameOver extends JPanel{
              System.exit(0);
          });
         this.add(this.exit);
- 
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            // Draw image
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 }
